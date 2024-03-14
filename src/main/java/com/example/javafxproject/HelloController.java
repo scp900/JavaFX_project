44204ed7a56551e55c67f9e475115319110ab853
Your Name <you@example.com>
2024-03-14 13:27:38 -0400

    Initial commit

---
+++ src/main/java/com/example/javafxproject/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.javafxproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void dwightsmelly() {
+        welcomeText.setText("Dwight really is smelly.");
+    }
+}
