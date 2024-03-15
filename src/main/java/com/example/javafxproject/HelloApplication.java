b9f82b3cdc916507aec9d9645773f1074a02f65e
Your Name <you@example.com>
2024-03-15 14:05:35 -0400

    Login is working

--- src/main/java/com/example/javafxproject/HelloApplication.java
+++ src/main/java/com/example/javafxproject/HelloApplication.java
@@ -4,15 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+import java.util.HashMap;
 
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Login");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/javafxproject/HelloController.java
+++ src/main/java/com/example/javafxproject/HelloController.java
@@ -3,18 +3,45 @@ package com.example.javafxproject;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private TextField Username;
+    @FXML
+    private TextField Password;
+
+    public HashMap<String, String> logins = new HashMap<String, String>();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void onLogin() {
+        String enteredUsername = Username.getText();
+        String enteredPassword = Password.getText();
+
+        logins.put("Username","Password");
+        logins.put("Username2","Password");
+
+        if (logins.containsKey(enteredUsername)){
+            System.out.println("Username Found");
+            if (Objects.equals(logins.get(enteredUsername), enteredPassword)){
+                System.out.println("Password is correct");
+            }else{
+                System.out.println("Password is incorrect");
+            }
+        }else{
+            System.out.println("Username Not Found");
+            welcomeText.setText("Username Not Found");
+        }
     }
 
     @FXML
-    protected void dwightsmelly() {
-        welcomeText.setText("Dwight really is smelly.");
+    protected void onExit() {
+        System.exit(0);
     }
+
+
 }
